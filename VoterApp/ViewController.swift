7226e48916a4000dcfbea29c9a50bfb9e78d7d07
Lorenzo Leon Robles <MacLuos@lorenzos-macbook-pro.local>
2016-09-14 22:15:41 -0500

    Initial Commit

---
+++ VoterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoterApp
+//
+//  Created by Lorenzo Leon Robles on 9/14/16.
+//  Copyright © 2016 Lorenzo Leon Robles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
